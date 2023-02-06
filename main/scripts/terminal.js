f1e5a722ace8ad532efea7139c829515f6acb089
Milo C <75094742+whotf1@users.noreply.github.com>
2023-02-06 15:14:20 -0600

    complete prompts relating to database functioins

--- main/scripts/terminal.js
+++ main/scripts/terminal.js
@@ -31,15 +31,18 @@ const mainMenu = async (select) => {
       break;
     case "view_departments":
       await viewDepartments();
-      break;
-    case "employee_salaries":
-      await employeeSalaries();
+      break; 
+    case "department_salaries":
+      await departmentSalaries();
       break;
     case "add_new_department":
       await addNewDepartment();
       break;
     case "delete_employee":
       await deleteEmployee();
+      break;
+    case "delete_role":
+      await deleteRole();
       break;
     case "delete_department":
       await deleteDepartment();
@@ -56,7 +59,7 @@ const continueMenu = async () => {
   const resp = await INQ.prompt([
     {
       type:"confirm",
-      message:"continue?",
+      message:"continue? ",
       name:"continue",
     },
   ]);
@@ -89,7 +92,7 @@ const employeesUnderManager = async ()=> {
   const resp = await INQ.prompt([
     {
       type:"list",
-      message:"select a manager",
+      message:"select a manager: ",
       choices:managers,
       name:"manager",
     },
@@ -117,7 +120,7 @@ const employeesInDepartment = async ()=> {
   const resp = await INQ.prompt([
     {
       type:"list",
-      message:"select department you want to see employees from",
+      message:"select department you want to see employees from: ",
       choices: departments,
       name:"department",
     },
@@ -135,5 +138,341 @@ const employeesInDepartment = async ()=> {
   ]);
   console.table(rows2);
   await continueMenu();
-}
+};
 
+//add employee, inquirer wil get name role and their mananger name, and add them to the db accordingly
+
+const addEmployee = async ()=> {
+  const sql = "SELECT * FROM role";
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const roles = rows.map((role) => {
+    return {name: role.title, value:role.id};
+  });
+  const sql2 = "SELECT * FROM employee";
+  const [rows2,fields2] = await dbTunnel.execute(sql2);
+  const managers = rows2.map((manager) => {
+    return{name:manager.first_name, value:manager.id};
+  });
+  //add option to not ad a maanger 
+  managers.push({name:"No Manager", value:null});
+
+  const resp = await INQ.prompt([
+    {
+      type:"input",
+      message:"enter Employees first name: ",
+      name:"first_Name"
+    },
+    {
+      type:"input",
+      message:"enter Employees last name: ",
+      name:"last_name",
+    },
+    {
+      type:"list",
+      message:"select employees role: ",
+      choices:roles,
+      name:"role_id",
+    },
+    {
+      type:"list",
+      message:"select Employees manager: ",
+      choices:managers,
+      name:"manager_id",
+    }
+  ]);
+
+  const sql3 = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+  const params = [
+    resp.first_name,
+    resp.last_name,
+    resp.role_id,
+    resp.manager_id,
+  ];
+  const[rows3,fields3] = await dbTunnel.execute(sql3, params);
+  console.log("##=========== Employee Added ===========##");
+  await continueMenu();
+};
+
+const changeEmployeeRole = async ()=> {
+  const sql = "SELECT * FROM employee";
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const employees = rows.map((employee) => {
+    return {name:employee.first_name, value:employee.id};
+  });
+
+  const sql2 = "SELECT * FROM role";
+  const[rows2,fields2] = await dbTunnel.execute(sql2);
+  const roles = rows2.map((role)=> {
+    return { name: role.title, value: role.id};
+  });
+
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select employee: ",
+      choices:employees,
+      name:"employee_id",
+    },
+    {
+      type:"list",
+      message:"select employees new role: ",
+      choices:roles,
+      name: "role_id"
+    },
+  ]);
+
+  const sql3 = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  const params = [resp.role_id, resp.employee.id];
+  const [rows3,fields3] = await dbTunnel.execute(sql3, params);
+  console.log("##=========== Employee Role updated Succesfully ===========##");
+  await continueMenu();
+};
+
+//update employee manager, inquire user to give manager for employee
+
+const changeEmployeeManager = async ()=>{
+  const sql = `SELECT * FROM employee`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const employees = rows.map((employee) => {
+    return {name: employee.first_name, value:employee.id};
+  });
+  const sql2 = `SELECT * FROM employee`;
+  const [rows2,fields2] = await dbTunnel.execute(sql2);
+  const managers = rows2.map((manager) => {
+    return {name:manager.first_name, value: manager.id};
+  });
+
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select employee: ",
+      choices:employees,
+      name:"employee_id"
+    },
+    {
+      type:"list",
+      message:"select employees new manager: ",
+      choices: managers,
+      name: "manager_id"
+    }
+  ]);
+  const sql3 = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+  const params = [resp.manager._id, resp.employee_id];
+  const [rows3,fields3] = await dbTunnel.execute(sql3, params);
+  console.log("##=========== Employees manager updated ===========##");
+  await continuePrompt();
+};
+
+//view all rows in db
+
+const viewRoles = async() => {
+  const sql = `SELECT role.id, role.title, department.name AS department, role.salary
+  FROM role
+  LEFT JOIN department ON role.department_id = department.id`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  console.table(rows);
+  await continueMenu();
+};
+
+//add role to db, inq will get title salary and department
+const addNewRole = async () => {
+  const sql = `SELECT * FROM department`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const departments = rows.map((department) => {
+    return {name:department.name, value:department.id};
+  });
+
+  const resp = await INQ.prompt([
+    {
+      type:"input",
+      message:"Enter role title: ",
+      name:"title"
+    },
+    {
+      type:"input",
+      message:"Enter role salary: ",
+      name: "salary",
+    },
+    {
+      type:"list",
+      message:"select role department: ",
+      choices: departments,
+      name:"department_id",
+    }
+  ]);
+
+  const sql2 = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+  params = [resp.title, resp.salary, resp.department_id];
+  const [rows2,fields2] = await dbTunnel.execute(sql2, params);
+  console.log("##=========== Role added ===========##");
+  await continueMenu();
+};
+
+//view all departments in db
+
+const viewDepartments = async ()=>{
+  const sql = `SELECT * ALL department`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  console.table(rows);
+  await continueMenu();
+};
+
+//view department total salaries
+const departmentSalaries = async ()=>{
+  const sql = `SELECT * FROM department`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const departments = rows.map((department) => {
+    return {name:department.name, value:department.id};
+  });
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select department: ",
+      chpoces:departments,
+      name:"departments_id"
+    }
+  ]);
+  const sql2 = `SELECT department.name AS department, SUM(role.salary) AS total_salary, COUNT(employee.id) as total_employees
+  FROM employee
+  LEFT JOIN role ON employee.role_id = role.id
+  LEFT JOIN department ON role.department_id = department.id
+  WHERE department.id = ?
+  GROUP BY department.name`;
+  const params = [resp.department_id];
+  const [rows2,fields2] = await dbTunnel.execute(sql2,params);
+  console.table(rows2);
+  await continueMenu();
+};
+
+//add department to db
+const addNewDepartment = async ()=>{
+  const resp = await INQ.prompt([
+    {
+      type:"input",
+      message:"enter department name you want to add to departments: ",
+      name:"name",
+    }
+  ]);
+
+  const sql = `INSERT INTO department (name) VALUES (?)`;
+  const params = [resp.names];
+  const [rows,fields] = await dbTunnel.execute(sql,params);
+  console.log("##=========== Department added ===========##");
+  await continueMenu();
+};
+
+//delete employee from db
+const deleteEmployee = async ()=>{
+  const sql = `SELECT * FROM employee`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const employees = rows.map((employee) => {
+    return {name:employee.first_name, value:employee.id};
+  });
+
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select an employee to delete, THIS IS PERMANENT",
+      choices:employees,
+      name:"employees_id"
+    },
+    {
+      type:"confirm",
+      message:"are you sure you want to proceed? THIS IS PERMANENT!",
+      name:"confirm",
+      default:false
+    }
+  ]); // this should always need a confirm since it is permanent
+  //logic for confirm
+  if (!resp.confirm) {
+    console.log("##=========== Delete aborted ===========##")
+  } else{
+    const sql2 = `DELETE FROM employee WHERE id = ?`;
+    const params = [resp.employee_id];
+    const [rows2,fields2] = await dbTunnel.execute(sql2,params);
+    console.log("##=========== Delete Complete ===========##");
+  }
+  await continueMenu();
+};
+
+//delete role from db
+const deleteRole = async () => {
+  const sql = `SELECT * FROM role`;
+  const [rows,fields] = await dbTunnel.execute(sql);
+  const roles = rows.map((role) => {
+    return {name:role.title, value:role.id};
+  });
+
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select role to delete: ",
+      choices:roles,
+      name:"role_id"
+    },
+    {
+      type:"confirm",
+      message:"are you sure you want to delete this role? THIS IS PERMANENT",
+      name:"confirm",
+      default:false,
+    }
+  ]);
+  //logic for confirm delete of department
+  if(!resp.confirm){
+    console.log("##=========== Role delete aborted ===========##")
+  } else{
+    //delete all employees under role first
+    const sql1 = `DELETE FROM employee Where rold_id = ?`;
+    const params1 = [resp.role_id];
+    const [rows1,fields1] = await dbTunnel.execute(sql1,params1);
+    // then delete the role
+    const sql2 = `DELETE FROM role WHERE id = ?`;
+    const params = [resp.role_id];
+    const [rows2,fields2] = await dbTunnel.execute(sql2,params);
+    console.log("##=========== Role delete complete ===========##");
+  }
+  await continueMenu();
+};
+
+// delete department from db by department name
+
+const deleteDepartment = async()=>{
+  const sql = `SELECT * FROM department`;
+  const[rows,fields]=await dbTunnel.execute(sql);
+  const departments = rows.map((department)=>{
+    return{name:department.name,value:department.id};
+  });
+  const resp = await INQ.prompt([
+    {
+      type:"list",
+      message:"select department to delete: ",
+      choices:departments,
+      name:"department_id"
+    },
+    {
+      type:"confirm",
+      message:"are you sure you want to proceed? ",
+      name:"confirm",
+      default:false
+    }
+  ]);
+
+  //logic for the confirm
+  if(!resp.confirm){
+    console.log("##=========== Department delete aborted ===========##")
+  } else {
+    const sql1 = `DELETE FROM employee WHERE role_id IN (SELECT id FROM role WHERE department_id = ?)`;
+    const params1 = [resp.department_id];
+    const[rows1,fields1] = await dbTunnel.execute(sql1,params1);
+    //delete roles in department
+    const sql2 = `DELETE FROM role WHERE department_id = ?`;
+    const params2 = [resp.department_id];
+    const [rows2,fields2] = await dbTunnel.execute(sql2,params2);
+    //finally delete department
+    const sql3 = `DELETE FROM department WHERE id = ?`;
+    const params3 = [resp.department_id];
+    const [rows3,fields3] = await dbTunnel.execute(sql3,params3);
+    console.log("##=========== Department delete complete ===========##");
+  }
+  await continueMenu();
+}
